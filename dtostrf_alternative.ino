ad1b8438db78ed324617032a78a8c7f3c449c1b2
Sherzaad <57634578+sherzaad@users.noreply.github.com>
2024-02-24 09:04:05 +0000

    dtostrf_alternative.ino

--- dtostrf_alternative.ino
+++ dtostrf_alternative.ino
@@ -1,36 +1,42 @@
-void float2string(char *arr, float val, int8_t dp = 20) {
-  if (isnan(val)) {
-    strcat(arr, "NAN");
-    return;
-  }
-  else if (isinf(val)) {
-    strcat(arr, "INF");
-    return;
-  }
-  else if (val > 4294967040.0 || val < -4294967040.0) {
-    strcat(arr, "OVF"); // constant determined empirically
-    return;
+class float2string {
+public:
+  const static uint8_t f_str_max_len = 20;
+  inline static char f_str[f_str_max_len];
+
+  static void make(float val, uint8_t dp = 20) {
+    f2str(f_str, val, (int8_t)dp);
   }
 
-  if (dp > 0) {
-    float f = abs(val);
-    long whole_num = f; //casting float to long integer(whole number part of number)
-    float frac = f - whole_num; //get the fractional part of number
-
-    //turning fractional part of number into an integer value
-    while (dp-- > 0) {
-      frac *= 10;
+private:
+  static void f2str(char *arr, float val, int8_t dp) {
+    if (isnan(val)) {
+      memcpy(arr, "NAN", f_str_max_len);
+      return;
+    } else if (isinf(val)) {
+      memcpy(arr, "INF", f_str_max_len);
+      return;
+    } else if (val > 4294967040.0 || val < -4294967040.0) {
+      memcpy(arr, "OVF", f_str_max_len); // constant determined empirically
+      return;
     }
-    frac += 0.5; //roundup
-    sprintf(arr, "%ld.%ld",(long)val, (long)frac);
-  }
-  else {
-    sprintf(arr, "%ld", (long)(val + (val > 0 ? 0.5 : -0.5)));
-  }
-}
 
+    if (dp > 0) {
+      float f = abs(val);
+      long whole_num = f;          //casting float to long integer(whole number part of number)
+      float frac = f - whole_num;  //get the fractional part of number
 
-char str[20]; //maximum number of characters in c-string + 2 (for decimal point and null terminator)
+      //turning fractional part of number into an integer value
+      while (dp-- > 0) {
+        frac *= 10;
+      }
+      frac += 0.5;  //roundup
+      sprintf(arr, "%ld.%ld", (long)val, (long)frac);
+    } else {
+      sprintf(arr, "%ld", (long)(val + (val > 0 ? 0.5 : -0.5)));
+    }
+  }
+};
+
 float f = 3.141592;
 int cnt = 0;
 
@@ -38,17 +44,15 @@ void setup() {
   Serial.begin(115200);
 }
 void loop() {
-  str[0] = '\0'; //clear string
-
   Serial.print("Try: ");
   Serial.print(cnt++, DEC);
 
   //sprintf(str,String(f, 4).c_str()); //BEWARE!'String' may slowly eating away SRAM memory
 
-  float2string(str, f, 4);
+  float2string::make(f, 4);
 
   Serial.print(", f: ");
-  Serial.println(str);
+  Serial.println(float2string::f_str);
 
   //increment the float number
   f += 0.1;
